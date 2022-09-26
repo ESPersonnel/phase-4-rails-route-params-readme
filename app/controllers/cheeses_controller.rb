ead0890c80aaa2742844038f874969e44c133e2c
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-09-26 08:54:16 +0300

    Done.

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,13 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    # Find a cheese using the id from the URL
+    # Send a JSON response with the cheese
+    cheese = Cheese.find(params[:id])
+    render json: cheese
+    # Using find_by_id is the same as using find
+    # cheese = Cheese.find_by_id(params[:id])
+  end
+
 end
